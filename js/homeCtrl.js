cbcbcd172ff4e51783d709e9e406f258530ea786
Tyler Palmer <mr.tylerpalmer@gmail.com>
2015-12-14 19:18:12 -0700

    Finished day one of DevMtIn Angular assignment

---
+++ js/homeCtrl.js
@@ -0,0 +1,19 @@
+angular.module('devMtIn').controller('homeCtrl', function($scope) {
+
+  $scope.myProfile = {
+      name: 'Tyler Palmer',
+      friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
+  };
+
+  $scope.sortOptions = [
+    {
+      display: 'Ascending',
+      value: false
+    },
+    {
+      display: 'Descending',
+      value: true
+    }
+  ];
+
+});
